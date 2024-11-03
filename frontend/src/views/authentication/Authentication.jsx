1ffe53bb5af28579f22ab2b876db0cf752ebe071
Terfiel-22 <dietherfiel@gmail.com>
2024-11-03 14:50:19 +0800

    minor update

--- frontend/src/views/authentication/Authentication.jsx
+++ frontend/src/views/authentication/Authentication.jsx
@@ -1,101 +1,68 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { Grid, Box, Stack, Typography } from '@mui/material';
+import { Grid, Box, Stack, Typography, Card } from '@mui/material';
 import PageContainer from 'src/components/container/PageContainer';
-import img1 from 'src/assets/images/backgrounds/login-bg.svg';
 import Logo from 'src/layouts/full/shared/logo/Logo';
 import AuthLogin from './authForms/AuthLogin';
 
 const Authentication = () => {
     return (
         <PageContainer title="JuanDepEd | Authentication" description="Juan DepEd Authentication Page">
-            <Grid container spacing={0} sx={{ overflowX: 'hidden' }}>
-                <Grid
-                    item
-                    xs={12}
-                    sm={12}
-                    lg={7}
-                    xl={8}
-                    sx={{
-                        position: 'relative',
-                        '&:before': {
-                            content: '""',
-                            background: 'radial-gradient(#d2f1df, #d3d7fa, #bad8f4)',
-                            backgroundSize: '400% 400%',
-                            animation: 'gradient 15s ease infinite',
-                            position: 'absolute',
-                            height: '100%',
-                            width: '100%',
-                            opacity: '0.3',
-                        },
-                    }}
-                >
-                    <Box position="relative">
-                        <Box px={3}>
-                            <Logo />
-                        </Box>
-                        <Box
-                            alignItems="center"
-                            justifyContent="center"
-                            height={'calc(100vh - 75px)'}
-                            sx={{
-                                display: {
-                                    xs: 'none',
-                                    lg: 'flex',
-                                },
-                            }}
-                        >
-                            <img
-                                src={img1}
-                                alt="bg"
-                                style={{
-                                    width: '100%',
-                                    maxWidth: '500px',
-                                }}
+            <Box
+                sx={{
+                    position: 'relative',
+                    '&:before': {
+                        content: '""',
+                        background: 'radial-gradient(#d2f1df, #d3d7fa, #bad8f4)',
+                        backgroundSize: '400% 400%',
+                        animation: 'gradient 15s ease infinite',
+                        position: 'absolute',
+                        height: '100%',
+                        width: '100%',
+                        opacity: '0.3',
+                    },
+                }}
+            >
+                <Grid container spacing={0} justifyContent="end" sx={{ height: '100vh' }}>
+                    <Grid
+                        item
+                        xs={12}
+                        sm={12}
+                        lg={5}
+                        xl={4}
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                        p={2}
+                    >
+                        <Card elevation={9} sx={{ p: 4, zIndex: 1, width: '100%', maxWidth: '450px' }}>
+                            <Box display="flex" alignItems="center" justifyContent="center">
+                                <Logo />
+                            </Box>
+                            <AuthLogin
+                                subtitle={
+                                    <Stack direction="row" spacing={1} justifyContent="center" mt={3}>
+                                        <Typography color="textSecondary" variant="h6" fontWeight="500">
+                                            New to JuanDepEd?
+                                        </Typography>
+                                        <Typography
+                                            component={Link}
+                                            to="/auth/register"
+                                            fontWeight="500"
+                                            sx={{
+                                                textDecoration: 'none',
+                                                color: 'primary.main',
+                                            }}
+                                        >
+                                            Create an account
+                                        </Typography>
+                                    </Stack>
+                                }
                             />
-                        </Box>
-                    </Box>
+                        </Card>
+                    </Grid>
                 </Grid>
-                <Grid
-                    item
-                    xs={12}
-                    sm={12}
-                    lg={5}
-                    xl={4}
-                    display="flex"
-                    justifyContent="center"
-                    alignItems="center"
-                >
-                    <Box p={4}>
-                        <AuthLogin
-                            title="Welcome to JuanDepEd"
-                            subtext={
-                                <Typography variant="subtitle1" color="textSecondary" mb={1}>
-                                    Your Admin Dashboard
-                                </Typography>
-                            }
-                            subtitle={
-                                <Stack direction="row" spacing={1} mt={3}>
-                                    <Typography color="textSecondary" variant="h6" fontWeight="500">
-                                        New to JuanDepEd?
-                                    </Typography>
-                                    <Typography
-                                        component={Link}
-                                        to="/auth/register"
-                                        fontWeight="500"
-                                        sx={{
-                                            textDecoration: 'none',
-                                            color: 'primary.main',
-                                        }}
-                                    >
-                                        Create an account
-                                    </Typography>
-                                </Stack>
-                            }
-                        />
-                    </Box>
-                </Grid>
-            </Grid>
+            </Box>
         </PageContainer>
     )
 }
