615f073123c17b5c2168ec59b30ecda75a45c5bc
Terfiel-22 <dietherfiel@gmail.com>
2024-11-04 18:35:08 +0800

    Setup Server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("Welcome to Juan DepEd");
+import { config } from "dotenv";
+import express from "express";
+
+/** Initialization */
+config();
+
+/** Variables */
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+/** Middlewares */
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}.`);
+});
