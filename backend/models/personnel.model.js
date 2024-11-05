aae31804d9a4516ca2f98bcc6e4840babd6eff64
Terfiel-22 <dietherfiel@gmail.com>
2024-11-05 12:57:27 +0800

    Personnel Model

--- backend/models/personnel.model.js
+++ backend/models/personnel.model.js
@@ -70,13 +70,66 @@ import { model, Schema } from "mongoose";
     status -> Status
  */
 
-// Personnel Schema
 const personnelSchema = new Schema(
   {
-    first_day: {
-      type: String,
-      required: true,
-      index: true,
+    basicInformation: {
+      empNo: { type: String, default: "" },
+      account: { type: String, default: "" },
+      lName: { type: String, default: "" },
+      fName: { type: String, default: "" },
+      mName: { type: String, default: "" },
+      civilStatus: { type: String, default: "" },
+      position: { type: String, default: "" },
+      eligibility: { type: String, default: "" },
+      birthdate: { type: Date },
+      age: { type: Number, default: 0 },
+      bloodType: { type: String, default: "" },
+      yrsOfTeachingPrivate: { type: Number, default: 0 },
+      tin: { type: String, default: "" },
+      gsisId: { type: String, default: "" },
+      hdmfId: { type: String, default: "" },
+      phicId: { type: String, default: "" },
+      contactNo: { type: String, default: "" },
+      depedEmail: { type: String, default: "" },
+      permAddress: { type: String, default: "" },
+      resAddress: { type: String, default: "" },
+      prcIdNo: { type: String, default: "" },
+      validityDate: { type: Date },
+      emergencyContact: { type: String, default: "" },
+      relationship: { type: String, default: "" },
+      phoneNo: { type: String, default: "" },
+    },
+    employment: {
+      plantilla: { type: String, default: "" },
+      origApt: { type: Date },
+      grade: { type: String, default: "" },
+      track: { type: String, default: "" },
+      subject: { type: String, default: "" },
+      loyaltyCounter: { type: Number, default: 0 },
+      lastAward: { type: Date },
+      lastProm: { type: Date },
+      lastStep: { type: Date },
+      stepCounter: { type: Number, default: 0 },
+      salaryGrade: { type: Number, default: 0 },
+      step: { type: Number, default: 0 },
+      basicSalary: { type: Number, default: 0 },
+    },
+    educationalAttainment: {
+      bachelor: {
+        batch: { type: String, default: "" },
+        school: { type: String, default: "" },
+        unitsEarned: { type: Number, default: 0 },
+      },
+      masteral: {
+        batch: { type: String, default: "" },
+        school: { type: String, default: "" },
+        unitsEarned: { type: Number, default: 0 },
+      },
+      doctoral: {
+        batch: { type: String, default: "" },
+        school: { type: String, default: "" },
+        unitsEarned: { type: Number, default: 0 },
+      },
     },
   },
   { timestamps: true }
