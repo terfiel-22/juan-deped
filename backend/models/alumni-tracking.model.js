1f4491aa151abb97ae4e4830ed50e893a183592e
Terfiel-22 <dietherfiel@gmail.com>
2024-11-05 19:38:25 +0800

    minor changes

--- backend/models/alumni-tracking.model.js
+++ backend/models/alumni-tracking.model.js
@@ -1,7 +1,8 @@
-import { model, Schema } from "mongoose";
+import { model, Schema, Types } from "mongoose";
 
 const alumniTrackingSchema = new Schema(
   {
+    alumniId: { type: Types.ObjectId, required: true },
     currentStatus: {
       status: {
         type: String,
