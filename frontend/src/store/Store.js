a876e0bc8eb120864ae42398b3f99653734dd51c
Terfiel-22 <dietherfiel@gmail.com>
2024-11-06 23:02:34 +0800

    Persist Redux Data

--- frontend/src/store/Store.js
+++ frontend/src/store/Store.js
@@ -1,8 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './RootReducer';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export default store;
+export const persistor = persistStore(store);
