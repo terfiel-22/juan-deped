9cc992eb7340b1d217153f8c42bd3fcc564c9f9b
Terfiel-22 <dietherfiel@gmail.com>
2024-11-06 22:52:39 +0800

    Leverage combineReducers of Redux

---
+++ frontend/src/store/RootReducer.js
@@ -0,0 +1,26 @@
+import { combineReducers } from '@reduxjs/toolkit';
+import CustomizerReducer from './customizer/CustomizerSlice';
+import ChatsReducer from './apps/chat/ChatSlice';
+import NotesReducer from './apps/notes/NotesSlice';
+import EmailReducer from './apps/email/EmailSlice';
+import TicketReducer from './apps/tickets/TicketSlice';
+import ContactsReducer from './apps/contacts/ContactSlice';
+import EcommerceReducer from './apps/eCommerce/EcommerceSlice';
+import UserProfileReducer from './apps/userProfile/UserProfileSlice';
+import BlogReducer from './apps/blog/BlogSlice';
+import { UserReducer } from './user/UserSlice';
+
+export const rootReducer = combineReducers({
+  customizer: CustomizerReducer,
+  chatReducer: ChatsReducer,
+  emailReducer: EmailReducer,
+  notesReducer: NotesReducer,
+  contactsReducer: ContactsReducer,
+  ticketReducer: TicketReducer,
+  ecommerceReducer: EcommerceReducer,
+  userpostsReducer: UserProfileReducer,
+  blogReducer: BlogReducer,
+
+  /** JuanDepEd */
+  userReducer: UserReducer,
+});
--- frontend/src/store/Store.js
+++ frontend/src/store/Store.js
@@ -1,30 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import CustomizerReducer from './customizer/CustomizerSlice';
-import ChatsReducer from './apps/chat/ChatSlice';
-import NotesReducer from './apps/notes/NotesSlice';
-import EmailReducer from './apps/email/EmailSlice';
-import TicketReducer from './apps/tickets/TicketSlice';
-import ContactsReducer from './apps/contacts/ContactSlice';
-import EcommerceReducer from './apps/eCommerce/EcommerceSlice';
-import UserProfileReducer from './apps/userProfile/UserProfileSlice';
-import BlogReducer from './apps/blog/BlogSlice';
-import { UserReducer } from './user/UserSlice';
+import { rootReducer } from './RootReducer';
 
 export const store = configureStore({
-  reducer: {
-    customizer: CustomizerReducer,
-    chatReducer: ChatsReducer,
-    emailReducer: EmailReducer,
-    notesReducer: NotesReducer,
-    contactsReducer: ContactsReducer,
-    ticketReducer: TicketReducer,
-    ecommerceReducer: EcommerceReducer,
-    userpostsReducer: UserProfileReducer,
-    blogReducer: BlogReducer,
-
-    /** JuanDepEd */
-    userReducer: UserReducer,
-  },
+  reducer: rootReducer,
 });
 
 export default store;
