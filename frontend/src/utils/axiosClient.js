a1337581a4c98b7688e9b9fe5f7f4a7392b31d3a
Terfiel-22 <dietherfiel@gmail.com>
2024-11-06 20:15:07 +0800

    Fix Cors Policy Error

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: `${import.meta.env.VITE_SERVER_BASE_URI}/api`,
+  baseURL: '/api',
   withCredentials: true,
 });
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -35,10 +35,11 @@ export default defineConfig({
       ],
     },
   },
-
-  // plugins: [react(),svgr({
-  //   exportAsDefault: true
-  // })],
-
   plugins: [svgr(), react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': { target: 'http://localhost:8000' },
+    },
+  },
 });
