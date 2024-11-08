41e17f4d92f8a2c717f5dad3a46055b46535d905
Terfiel-22 <dietherfiel@gmail.com>
2024-11-08 13:10:46 +0800

    UI for Personnel Table List

--- frontend/src/views/user-management/personnels/Personnels.jsx
+++ frontend/src/views/user-management/personnels/Personnels.jsx
@@ -1,6 +1,28 @@
+import { Box } from "@mui/system"
+import PageContainer from "../../../components/container/PageContainer"
+import Breadcrumb from "../../../layouts/full/shared/breadcrumb/Breadcrumb"
+import PersonnelTableList from "../../../components/user-management/personnels/PersonnelTableList";
+
+const BCrumb = [
+    {
+        to: '/personnels',
+        title: 'User Management',
+    },
+    {
+        title: 'Personnels',
+    },
+];
+
 const Personnels = () => {
     return (
-        <div>Personnels</div>
+        <PageContainer title="Personnels" description="this is Personnels page">
+            {/* breadcrumb */}
+            <Breadcrumb title="Personnels" items={BCrumb} />
+            {/* end breadcrumb */}
+            <Box>
+                <PersonnelTableList />
+            </Box>
+        </PageContainer>
     )
 }
 
