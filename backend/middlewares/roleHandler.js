129e3522bb78aa2eda99a3d39593ec3a6d3719da
Terfiel-22 <dietherfiel@gmail.com>
2024-11-20 00:54:38 +0800

    Add middleware for admin only

--- backend/middlewares/roleHandler.js
+++ backend/middlewares/roleHandler.js
@@ -3,6 +3,8 @@ import HttpError from "../utils/HttpError.utils.js";
 import { config } from "dotenv";
 import Auth from "../models/auth.model.js";
 import merge from "lodash/merge.js";
+import get from "lodash/lodash.js";
+import { USER_ROLES } from "../enums/UserRole.js";
 config();
 
 const { JWT_SECRET_KEY } = process.env;
@@ -10,15 +12,30 @@ const { JWT_SECRET_KEY } = process.env;
 export const isAuthenticated = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
-    if (!token) throw new HttpError("Unauthorized - No Token Provided", 401);
+    if (!token) throw new HttpError("Unauthorized - No Token Provided.", 401);
 
     const decoded = jwt.verify(token, JWT_SECRET_KEY);
-    if (!decoded) throw new HttpError("Unauthorized - Invalid Token", 401);
+    if (!decoded) throw new HttpError("Unauthorized - Invalid Token.", 401);
 
     const user = await Auth.findById(decoded.userId).select("-password");
-    if (!user) throw new HttpError("User not found", 404);
+    if (!user) throw new HttpError("User not found.", 404);
 
     merge(req, { user });
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const isAdmin = async (req, res, next) => {
+  try {
+    const user = get(req, "user");
+    if (!user)
+      throw new HttpError("Unauthorized - User is not authenticated.", 401);
+
+    if (user.role != USER_ROLES.Admin)
+      throw new HttpError("Unauthorized - User is not an admin.", 401);
 
     next();
   } catch (error) {
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -4,9 +4,10 @@ import {
   logout,
   fetchAuths,
 } from "../controllers/auth.controller.js";
+import { isAdmin, isAuthenticated } from "../middlewares/roleHandler.js";
 
 export default (router) => {
-  router.get("/auths", fetchAuths);
+  router.get("/auths", isAuthenticated, isAdmin, fetchAuths);
   router.post("/auth/register", register);
   router.post("/auth/login", login);
   router.post("/auth/logout", logout);
