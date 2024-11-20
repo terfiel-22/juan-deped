2299295fc1c0ffdbede99c01de19644af7c45fb7
Terfiel-22 <dietherfiel@gmail.com>
2024-11-20 22:33:37 +0800

    minor fix

--- frontend/src/views/user-management/accounts/AccountDialog.jsx
+++ frontend/src/views/user-management/accounts/AccountDialog.jsx
@@ -46,6 +46,7 @@ const AccountDialog = ({ isOpen, isFullScreen, handleClose: close, data = data ?
     return (
         <Dialog
             fullScreen={isFullScreen}
+            closeAfterTransition={false}
             open={isOpen}
             onClose={handleClose}
             aria-labelledby="responsive-dialog-title"
