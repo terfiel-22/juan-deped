dcdcbbe1d25c54a2e07a41432fd943dd4c0baad3
Terfiel-22 <dietherfiel@gmail.com>
2024-11-20 23:51:41 +0800

    Edit Auth on backend.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -142,3 +142,47 @@ export const addAuth = async (req, res, next) => {
     next(error);
   }
 };
+
+export const editAuth = async (req, res, next) => {
+  try {
+    const { _id, username, email, password, cpassword, role } = req.body;
+
+    if (!_id | !username | !email | !role)
+      throw new HttpError("Fill all required data.", 400);
+
+    const userByEmail = await Auth.findOne({ email });
+    if (userByEmail && userByEmail._id.toString() !== _id)
+      throw new HttpError("Email address already exist.", 400);
+
+    let values = {
+      username,
+      email,
+      role,
+    };
+
+    if (password) {
+      if (password !== cpassword)
+        throw new HttpError("Password confirmation does not matched.", 400);
+
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      values = {
+        ...values,
+        password: hashedPassword,
+      };
+    }
+
+    const updatedAuth = await Auth.findByIdAndUpdate(_id, values, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedAuth) throw new HttpError("User not found.", 404);
+
+    return res
+      .status(200)
+      .json({ message: "Account is successfully updated.", user: updatedAuth });
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -4,12 +4,14 @@ import {
   logout,
   fetchAuths,
   addAuth,
+  editAuth,
 } from "../controllers/auth.controller.js";
 import { isAdmin, isAuthenticated } from "../middlewares/roleHandler.js";
 
 export default (router) => {
   router.get("/auths", isAuthenticated, isAdmin, fetchAuths);
   router.post("/auth/add", isAuthenticated, isAdmin, addAuth);
+  router.put("/auth/edit", isAuthenticated, isAdmin, editAuth);
   router.post("/auth/register", register);
   router.post("/auth/login", login);
   router.post("/auth/logout", logout);
