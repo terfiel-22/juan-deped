9ad94004e9ba43741d35fbbb3cf24414f088a390
taki-fimito <jessabanawa@gmail.com>
2024-11-27 15:04:47 +0800

    minor update

--- frontend/src/components/tables/CustomMUIDataTable.jsx
+++ frontend/src/components/tables/CustomMUIDataTable.jsx
@@ -1,34 +1,59 @@
-import MUIDataTable from "mui-datatables";
-
+import { IconButton, Tooltip } from '@mui/material';
+import { IconEye } from '@tabler/icons';
+import MUIDataTable from 'mui-datatables';
 
 const transformData = (backendData) => {
-
     function isDate(value) {
         return !isNaN(Date.parse(value)) && new Date(value).toISOString() === value;
     }
 
+    const handleView = (rowIndex) => {
+        console.log(backendData[rowIndex])
+    }
+
+    const customColumn = {
+        name: 'ACTIONS',
+        options: {
+            customBodyRenderLite: (dataIndex, rowIndex) => {
+                return (
+                    <Tooltip title="View">
+                        <IconButton size="small" onClick={() => handleView(rowIndex)}>
+                            <IconEye size="1.1rem" />
+                        </IconButton>
+                    </Tooltip>
+                );
+            },
+            print: false,
+            download: false,
+            filter: false,
+            searchable: false,
+            sort: false,
+        },
+    };
+
     const columns = Object.keys(backendData[0]).map((key) => {
-        if (key === "_id") {
-            return { name: "_ID", options: { display: "excluded" } }
+        if (key === '_id') {
+            return { name: '_ID', options: { display: 'excluded' } };
         }
-        return key.replace(/([A-Z])/g, "_$1").toUpperCase();
+        return key.replace(/([A-Z])/g, '_$1').toUpperCase();
     });
+    columns.push(customColumn);
 
     const data = backendData.map((item) =>
         Object.values(item).map((value) => {
             if (isDate(value)) {
                 const date = new Date(value);
-                return date.toLocaleDateString("en-US", {
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric",
+                return date.toLocaleDateString('en-US', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
                 });
             }
             return value;
-        })
+        }),
     );
     return { columns, data };
-}
+};
 
 const CustomMUIDataTable = ({ title, backendData }) => {
     const OPTIONS = {
@@ -37,23 +62,17 @@ const CustomMUIDataTable = ({ title, backendData }) => {
         print: true,
         viewColumns: true,
         filter: true,
-        filterType: "dropdown",
-        responsive: "standard",
+        selectableRows: "none",
+        filterType: 'dropdown',
+        responsive: 'standard',
         rowHover: false,
         rowsPerPage: 5,
         rowsPerPageOptions: [5, 20, 50, 100],
-        onRowsDelete: (rowsDeleted) => {
-            const idsToDelete = rowsDeleted.data.map(d => backendData[d.dataIndex]._id);
-            if (confirm("Are you sure you want to delete?")) {
-                alert("Deleted successfully.");
-                console.log(idsToDelete);
-            }
-        }
     };
 
     if (!backendData) return;
 
-    const { columns, data } = transformData(backendData)
+    const { columns, data } = transformData(backendData);
 
     return (
         <MUIDataTable
@@ -64,6 +83,6 @@ const CustomMUIDataTable = ({ title, backendData }) => {
             className="print-container"
         />
     );
-}
+};
 
-export default CustomMUIDataTable
+export default CustomMUIDataTable;
