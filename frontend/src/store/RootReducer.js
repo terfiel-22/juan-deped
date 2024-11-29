27a03bb0acf3f17ce2b36902b1d2b81ca3711a45
migoiarmando <ashurasyntax@gmail.com>
2024-11-30 01:58:57 +0800

    Minor update

--- frontend/src/store/RootReducer.js
+++ frontend/src/store/RootReducer.js
@@ -1,17 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import CustomizerReducer from './customizer/CustomizerSlice';
 import { UserReducer } from './user/UserSlice';
-import { CareerReducer } from './career/CareerSlice';
-import { StudentReducer } from './student/StudentSlice';
 import { TableReducer } from './tables/TableReducer';
 
 export const rootReducer = combineReducers({
   customizer: CustomizerReducer,
   /** JuanDepEd */
   userReducer: UserReducer,
-  studentReducer: StudentReducer,
-  careerReducer: CareerReducer,
-
-  /** New */
   tableReducer: TableReducer,
 });
