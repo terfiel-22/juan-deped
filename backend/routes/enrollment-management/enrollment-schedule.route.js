12ccbce20743cc150d86cad989b6daa250f54a36
migoiarmando <ashurasyntax@gmail.com>
2024-11-30 17:52:20 +0800

    minor update

--- backend/routes/enrollment-management/enrollment-schedule.route.js
+++ backend/routes/enrollment-management/enrollment-schedule.route.js
@@ -4,13 +4,15 @@ import {
   editEnrollmentSchedule,
   fetchEnrollmentSchedules,
 } from "../../controllers/enrollment-management/enrollment-schedule.controller.js";
+import { isPersonnel } from "../../middlewares/roleHandler.js";
 
 export default (router) => {
-  router.post("/enrollment/schedule", createEnrollmentSchedule);
-  router.get("/enrollment/schedules", fetchEnrollmentSchedules);
-  router.put("/enrollment/schedule", editEnrollmentSchedule);
+  router.post("/enrollment/schedule", isPersonnel, createEnrollmentSchedule);
+  router.get("/enrollment/schedules", isPersonnel, fetchEnrollmentSchedules);
+  router.put("/enrollment/schedule", isPersonnel, editEnrollmentSchedule);
   router.delete(
     "/enrollment/schedule/:enrollmentScheduleId",
+    isPersonnel,
     deleteEnrollmentSchedule
   );
 };
