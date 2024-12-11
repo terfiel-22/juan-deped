500ca533b1d7d75a4144ee2e2daac55d996af491
terfiel-22 <dietherfiel@gmail.com>
2024-12-11 18:18:54 +0800

    minor update

--- frontend/src/views/enrollment-management/enhanced-beefs/EnhanceBeefDialog.jsx
+++ frontend/src/views/enrollment-management/enhanced-beefs/EnhanceBeefDialog.jsx
@@ -8,31 +8,94 @@ import useFetch from '../../../hooks/crud/useFetch';
 
 const MUI_X_PRODUCTS = [
     {
-        id: 'grid',
-        label: 'Data Grid',
+        id: 'genInfo',
+        label: 'General Information',
         children: [
-            { id: 'grid-community', label: '@mui/x-data-grid' },
-            { id: 'grid-pro', label: '@mui/x-data-grid-pro' },
-            { id: 'grid-premium', label: '@mui/x-data-grid-premium' },
+            { id: 'grid-community1', label: '@mui/x-data-grid' },
+            { id: 'grid-pro2', label: '@mui/x-data-grid-pro' },
+            { id: 'grid-premium3', label: '@mui/x-data-grid-premium' },
         ],
     },
     {
-        id: 'pickers',
-        label: 'Date and Time Pickers',
+        id: 'learnerInfo',
+        label: 'Learner Information',
         children: [
-            { id: 'pickers-community', label: '@mui/x-date-pickers' },
-            { id: 'pickers-pro', label: '@mui/x-date-pickers-pro' },
+            { id: 'grid-community4', label: '@mui/x-data-grid' },
+            { id: 'grid-pro5', label: '@mui/x-data-grid-pro' },
+            { id: 'grid-premium6', label: '@mui/x-data-grid-premium' },
         ],
     },
     {
-        id: 'charts',
-        label: 'Charts',
-        children: [{ id: 'charts-community', label: '@mui/x-charts' }],
+        id: 'addressInfo',
+        label: 'Address Information',
+        children: [
+            { id: 'grid-community7', label: '@mui/x-data-grid' },
+            { id: 'grid-pro8', label: '@mui/x-data-grid-pro' },
+            { id: 'grid-premium9', label: '@mui/x-data-grid-premium' },
+        ],
     },
     {
-        id: 'tree-view',
-        label: 'Tree View',
-        children: [{ id: 'tree-view-community', label: '@mui/x-tree-view' }],
+        id: 'parentGuardianInfo',
+        label: 'Parent/Guardian Information',
+        children: [
+            { id: 'grid-community10', label: '@mui/x-data-grid' },
+            { id: 'grid-pro11', label: '@mui/x-data-grid-pro' },
+            { id: 'grid-premium12', label: '@mui/x-data-grid-premium' },
+        ],
+    },
+    {
+        id: 'returningLearner',
+        label: 'For Returning Learner',
+        children: [
+            { id: 'grid-community13', label: '@mui/x-data-grid' },
+            { id: 'grid-pro14', label: '@mui/x-data-grid-pro' },
+            { id: 'grid-premium15', label: '@mui/x-data-grid-premium' },
+        ],
+    },
+    {
+        id: 'learnerInSHS',
+        label: 'For Learners in SHS',
+        children: [
+            { id: 'grid-community16', label: '@mui/x-data-grid' },
+            { id: 'grid-pro17', label: '@mui/x-data-grid-pro' },
+            { id: 'grid-premium18', label: '@mui/x-data-grid-premium' },
+        ],
+    },
+    {
+        id: 'pdlm',
+        label: 'Chosen Distance Learning Modalities',
+        children: [
+            { id: 'grid-community19', label: '@mui/x-data-grid' },
+            { id: 'grid-pro20', label: '@mui/x-data-grid-pro' },
+            { id: 'grid-premium21', label: '@mui/x-data-grid-premium' },
+        ],
+    },
+    {
+        id: 'ncPasser',
+        label: 'For NC Passer',
+        children: [
+            { id: 'grid-community22', label: '@mui/x-data-grid' },
+            { id: 'grid-pro23', label: '@mui/x-data-grid-pro' },
+            { id: 'grid-premium24', label: '@mui/x-data-grid-premium' },
+        ],
+    },
+    {
+        id: 'shsEligibility',
+        label: 'For SHS Eligibility',
+        children: [
+            { id: 'grid-community25', label: '@mui/x-data-grid' },
+            { id: 'grid-pro26', label: '@mui/x-data-grid-pro' },
+            { id: 'grid-premium27', label: '@mui/x-data-grid-premium' },
+        ],
+    },
+    {
+        id: 'addInfo',
+        label: 'Additional Information',
+        children: [
+            { id: 'grid-community28', label: '@mui/x-data-grid' },
+            { id: 'grid-pro29', label: '@mui/x-data-grid-pro' },
+            { id: 'grid-premium30', label: '@mui/x-data-grid-premium' },
+        ],
     },
 ];
 
